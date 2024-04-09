7aada841771d2ca8cb04e44b8701994e2691e76f
Manoj <manojkumarbonala3@gmail.com>
2024-04-09 12:58:54 +0530

    code

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { AppBar } from "./components/AppBar";
+import { Main } from "./pages/Main";
+
+function App() {
+  return (
+    <div>
+      <AppBar />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/AppBar.tsx
@@ -0,0 +1,24 @@
+import logo from "../assets/logo.png"
+import { Button } from "./ui/button"
+
+export const AppBar = () => {
+    return(
+        <>
+            <div className="flex justify-between items-center px-16 font-normal tracking-normal top-0 sticky bg-white ">
+                <div>
+                    <img src={logo} className="w-24 h-24 cursor-pointer"/>
+                </div>
+                <div>
+                    <ul className="flex gap-3 cursor-pointer text-lg font-medium items-center pr-8.5">
+                        <li className="hover:text-slate-400"><a href="#services">Services</a></li>
+                        <li className="hover:text-slate-400"><a href="#">About us</a></li>
+                        <li className="hover:text-slate-400">
+                        <Button className="bg-gradient-to-r from-red-900 to-red-900"> Contact us</Button>
+                        </li>
+                    </ul>
+                </div>
+                
+            </div>
+        </>
+    )
+}
